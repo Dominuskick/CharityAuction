f348063704290d0211d4ea220916ce228d90d876
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2024-02-10 06:50:19 +0200

    feat: implement screen for published lot

--- src/charity-auction-ui/src/components/Button/index.jsx
+++ src/charity-auction-ui/src/components/Button/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './button.module.css';
 
-const index = ({ children, isBlack, isWide }) => {
+const index = ({ children, isBlack, isWide, onClick }) => {
   return (
     <button
       className={`${styles.btn} ${isBlack ? styles.btn__black : undefined} ${
         isWide ? styles.btn__wide : undefined
       }`}
+      onClick={onClick}
     >
       {children}
     </button>
--- src/charity-auction-ui/src/pages/AccountCreateLot/index.jsx
+++ src/charity-auction-ui/src/pages/AccountCreateLot/index.jsx
@@ -3,8 +3,11 @@ import styles from './accountCreateLot.module.css';
 import { Header, Footer } from '@/layout';
 import Select from 'react-select';
 import { Button } from '@/components';
+import { Link } from 'react-router-dom';
 
 const index = () => {
+  const [isPublished, setIsPublished] = useState(false);
+
   const categoryOptions = [
     { value: 'Антикваріат', label: 'Антикваріат' },
     { value: 'Букіністика', label: 'Букіністика' },
@@ -44,79 +47,110 @@ const index = () => {
     }
   };
 
-  return (
-    <>
-      <Header />
-      <main className={styles.darkMain}>
-        <div className={styles.mainContent}>
-          <div className="responsiveWrapper">
-            <div className={styles.newLot}>
-              <h2 className={styles.header}>Створення аукціону</h2>
-              <div className={`${styles.inputWrapper} ${styles.inputName}`}>
-                <label>Вкажіть назву</label>
-                <input type="text" placeholder="Наприклад, картина з котами" />
-              </div>
-              <div className={`${styles.inputWrapper} ${styles.inputCategory}`}>
-                <label>Вкажіть категорію</label>
-                <Select
-                  placeholder="Категорія"
-                  options={categoryOptions}
-                  styles={customStyles}
-                  isMulti
-                />
-              </div>
-              <div className={styles.inputWrapper}>
-                <label>Додайте щонайменше 1 фото</label>
-                <div className={styles.photosWrapper}>
-                  {images.map((image, index) => (
-                    <div key={index} className={styles.photoInputWrapper}>
-                      <input
-                        type="file"
-                        accept="image/*"
-                        style={{ opacity: '0' }}
-                        onChange={(event) => onImageChange(index, event)}
-                      />
-                      {image ? (
-                        <img alt={`Картинка лота ${index + 1}`} src={image} />
-                      ) : index === 0 ? (
-                        <span>Додати фото</span>
-                      ) : (
-                        <span className={styles.photoIcon}></span>
-                      )}
+  if (!isPublished) {
+    return (
+      <>
+        <Header />
+        <main className={styles.darkMain}>
+          <div className={styles.mainContent}>
+            <div className="responsiveWrapper">
+              <div className={styles.newLot}>
+                <h2 className={styles.header}>Створення аукціону</h2>
+                <div className={`${styles.inputWrapper} ${styles.inputName}`}>
+                  <label>Вкажіть назву</label>
+                  <input
+                    type="text"
+                    placeholder="Наприклад, картина з котами"
+                  />
+                </div>
+                <div
+                  className={`${styles.inputWrapper} ${styles.inputCategory}`}
+                >
+                  <label>Вкажіть категорію</label>
+                  <Select
+                    placeholder="Категорія"
+                    options={categoryOptions}
+                    styles={customStyles}
+                    isMulti
+                  />
+                </div>
+                <div className={styles.inputWrapper}>
+                  <label>Додайте щонайменше 1 фото</label>
+                  <div className={styles.photosWrapper}>
+                    {images.map((image, index) => (
+                      <div key={index} className={styles.photoInputWrapper}>
+                        <input
+                          type="file"
+                          accept="image/*"
+                          style={{ opacity: '0' }}
+                          onChange={(event) => onImageChange(index, event)}
+                        />
+                        {image ? (
+                          <img alt={`Картинка лота ${index + 1}`} src={image} />
+                        ) : index === 0 ? (
+                          <span>Додати фото</span>
+                        ) : (
+                          <span className={styles.photoIcon}></span>
+                        )}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div className={styles.inputWrapper}>
+                  <label>Придумайте опис</label>
+                  <textarea placeholder="Опис" />
+                </div>
+                <div className={styles.row}>
+                  <div className={styles.inputWrapper}>
+                    <label>Початкова ціна</label>
+                    <div className={styles.bidInput}>
+                      <input type="number" placeholder="Ваша ставка" />
+                      <span>грн</span>
                     </div>
-                  ))}
-                </div>
-              </div>
-              <div className={styles.inputWrapper}>
-                <label>Придумайте опис</label>
-                <textarea placeholder="Опис" />
-              </div>
-              <div className={styles.row}>
-                <div className={styles.inputWrapper}>
-                  <label>Початкова ціна</label>
-                  <div className={styles.bidInput}>
-                    <input type="number" placeholder="Ваша ставка" />
-                    <span>грн</span>
+                  </div>
+                  <div className={styles.inputWrapper}>
+                    <label>Мінімальний крок ставки</label>
+                    <div className={styles.bidInput}>
+                      <input type="number" placeholder="Ваша ставка" />
+                      <span>грн</span>
+                    </div>
                   </div>
                 </div>
-                <div className={styles.inputWrapper}>
-                  <label>Мінімальний крок ставки</label>
-                  <div className={styles.bidInput}>
-                    <input type="number" placeholder="Ваша ставка" />
-                    <span>грн</span>
-                  </div>
+                <div className={styles.btnWrapper}>
+                  <Button onClick={() => setIsPublished(true)}>
+                    Виставити лот на аукціон
+                  </Button>
                 </div>
-              </div>
-              <div className={styles.btnWrapper}>
-                <Button>Виставити лот на аукціон</Button>
               </div>
             </div>
           </div>
-        </div>
-      </main>
-      <Footer />
-    </>
-  );
+        </main>
+        <Footer />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Header />
+        <main className={styles.darkMain}>
+          <div className={styles.mainContent}>
+            <div className="responsiveWrapper">
+              <div className={`${styles.newLot} ${styles.lotPublished}`}>
+                <h2>Дякуємо за участь в аукціоні!</h2>
+                <h3>Ваш лот стане доступним, після модерації</h3>
+                <div className={styles.btnWrapper}>
+                  <Link to={'/account'}>
+                    <Button>Повернутись до особистого кабінету</Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </>
+    );
+  }
 };
 
 export default index;
