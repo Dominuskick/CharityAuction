416a240bed2af31f6b83af309b665b1494ab6c90
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2024-02-28 22:05:59 +0200

    feat: implement hover effects for header links, labeled inputs and buttons also add disabled styles for button

--- src/charity-auction-ui/src/components/Button/index.jsx
+++ src/charity-auction-ui/src/components/Button/index.jsx
@@ -7,8 +7,9 @@ const index = ({ children, isBlack, isWide, onClick, disabled, loading }) => {
     <button
       className={`${styles.btn} ${isBlack && styles.btn__black} ${
         isWide && styles.btn__wide
-      } ${(disabled || loading) && styles.btn__disabled}`}
+      }`}
       onClick={onClick}
+      disabled={disabled || loading}
     >
       {loading ? <LoaderInline height={30} width={50} /> : children}
     </button>
